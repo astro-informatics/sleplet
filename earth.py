a8bb5205da68d6c7e2bbeb8650b34a8e54387fc9
paddyroddy <patrickjamesroddy@gmail.com>
2018-11-09 17:56:58 +0000

    change save directory

--- earth.py
+++ earth.py
@@ -31,5 +31,5 @@ if __name__ == '__main__':
 
     flm = earth()
     f = ssht.inverse(flm, sc.resolution, Reality=True)
-    filename = os.path.expanduser('~') + '/Dropbox/cosmoinformatics/earth.html'
+    filename = 'figures/earth.html'
     sc.plotly_plot(f, filename)
--- squashed_gaussian.py
+++ squashed_gaussian.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     # beta = np.pi  # theta
     beta = np.pi / 4
 
-    dir = os.path.expanduser('~') + '/Dropbox/cosmoinformatics/'
+    dir = 'figures/'
     # sc.plot(dir, alpha, beta, 'imag')  # north pole
     # sc.plot(dir, alpha, beta, 'imag', 'rotate')  # rotate
     sc.plot(dir, alpha, beta, 'real', 'translate')  # translate
