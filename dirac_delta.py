fix negative sign in convolution by using the conjugate

dirac_delta.py
@@ -35,9 +35,14 @@ def dirac_delta():
 
     # impose reality on flms
     for ell in range(L):
-        for m in range(-ell, ell + 1):
-            ind = ssht.elm2ind(ell, m)
-            flm[ind] = 1
+        m = 0
+        ind = ssht.elm2ind(ell, m)
+        flm[ind] = 1
+        for m in range(1, ell + 1):
+            ind_pm = ssht.elm2ind(ell, m)
+            ind_nm = ssht.elm2ind(ell, -m)
+            flm[ind_pm] = 1
+            flm[ind_nm] = (-1) ** m * np.conj(flm[ind_pm])
     return flm
 
 if __name__ == '__main__':

sifting_convolution.py
@@ -109,17 +109,41 @@ class SiftingConvolution(object):
             array -- translated flm
         '''
 
+        # compute pixels of \omega'
         pix_i = ssht.theta_to_index(beta, self.L)
-        pix_j = -ssht.phi_to_index(alpha, self.L)
+        pix_j = ssht.phi_to_index(alpha, self.L)
+
+        def helper(ind):
+            '''
+            computes the value of the spherical harmonic at omega'
+
+            Arguments:
+                flm {array} -- harmonic representation of function
+                ind {int} -- index of l & m
+
+            Returns:
+                float -- Y_{\ell m}(\omega')
+            '''
+
+            ylm_harmonic = np.zeros((self.L * self.L), dtype=complex)
+            ylm_harmonic[ind] = 1
+            ylm_pixel = ssht.inverse(ylm_harmonic, self.L)
+            ylm_omega = ylm_pixel[pix_i, pix_j]
+            if self.func_name == 'dirac_delta':
+                return np.conj(ylm_omega)
+            else:
+                return ylm_omega
 
         for ell in range(self.L):
             for m in range(-ell, ell + 1):
                 ind = ssht.elm2ind(ell, m)
-                ylm_harm = np.zeros((self.L * self.L), dtype=complex)
-                ylm_harm[ind] = 1
-                ylm_real = ssht.inverse(ylm_harm, self.L)
-                flm[ind] *= ylm_real[pix_i, pix_j]
-
+                if self.func_name == 'dirac_delta':
+                    flm[ind] = helper(ind)
+                else:
+                    if m == 0:
+                        flm[ind] *= helper(ind)
+                    else:
+                        flm[ind] = helper(ind)
         return flm
 
     def setup_plot(self, f, method='MW', close=True, parametric=False,