525833a4b1e2075d7181c5eca1e2ce2cc16801fe
paddyroddy <patrickjamesroddy@gmail.com>
2019-01-25 16:05:44 +0000

    project 2D Gaussian on the sphere

---
+++ elongated_gaussian.py
@@ -0,0 +1,57 @@
+from sifting_convolution import SiftingConvolution
+import pyssht as ssht
+import sys
+import os
+import numpy as np
+import yaml
+from argparse import ArgumentParser
+sys.path.append(os.path.join(os.environ['SSHT'], 'src', 'python'))
+
+
+def setup():
+    __location__ = os.path.realpath(os.path.join(
+        os.getcwd(), os.path.dirname(__file__)))
+    yaml_file = os.path.join(__location__, 'input.yml')
+    content = open(yaml_file)
+    config_dict = yaml.load(content)
+
+    parser = ArgumentParser(description='Create SSHT plot')
+    parser.add_argument('method', metavar='method', type=str, nargs='?', default='north', const='north', choices=[
+                        'north', 'rotate', 'translate'], help='plotting method i.e. north')
+    parser.add_argument('type', metavar='type', type=str, nargs='?', default='abs', const='north', choices=[
+                        'abs', 'real', 'imag', 'sum'], help='plotting method i.e. real')
+    parser.add_argument('--alpha', '-a', metavar='alpha', type=float,
+                        default=0.0, help='alpha/phi pi fraction')
+    parser.add_argument('--beta', '-b', metavar='beta', type=float,
+                        default=0.0, help='beta/theta pi fraction')
+    args = parser.parse_args()
+
+    return config_dict, args
+
+
+def grid_fun(x, y, x0=0, y0=0, x_sig=1, y_sig=1):
+    return np.exp(-(0.5 * ((x - x0) / x_sig) ** 2 + 0.5 * ((y - y0) / y_sig) ** 2))
+
+
+def elongated_gaussian():
+    config, args = setup()
+    L = config['L']
+    resolution = config['resolution']
+``
+    thetas, phis = ssht.sample_positions(resolution, Grid=True)
+    f = grid_fun(thetas, phis)
+    flm = ssht.forward(f, resolution, Reality=True)
+
+    return flm
+
+
+if __name__ == '__main__':
+    config, args = setup()
+
+    if 'method' not in config:
+        config['method'] = args.method
+    if 'plotting_type' not in config:
+        config['plotting_type'] = args.type
+
+    sc = SiftingConvolution(elongated_gaussian, config)
+    sc.plot(args.alpha, args.beta, reality=True)
