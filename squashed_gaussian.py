162d81334f0a1d2794755c1378233670559e2112
paddyroddy <patrickjamesroddy@gmail.com>
2019-01-28 12:51:18 +0000

    fix squashed Gaussian to work using pixel space

--- squashed_gaussian.py
+++ squashed_gaussian.py
@@ -1,11 +1,11 @@
+from sifting_convolution import SiftingConvolution
+import pyssht as ssht
 import sys
 import os
 import numpy as np
 import yaml
 from argparse import ArgumentParser
 sys.path.append(os.path.join(os.environ['SSHT'], 'src', 'python'))
-import pyssht as ssht
-from sifting_convolution import SiftingConvolution
 
 
 def setup():
@@ -14,35 +14,44 @@ def setup():
     yaml_file = os.path.join(__location__, 'input.yml')
     content = open(yaml_file)
     config_dict = yaml.load(content)
-    return config_dict
+
+    parser = ArgumentParser(description='Create SSHT plot')
+    parser.add_argument('method', metavar='method', type=str, nargs='?', default='north', const='north', choices=[
+                        'north', 'rotate', 'translate'], help='plotting method i.e. north')
+    parser.add_argument('type', metavar='type', type=str, nargs='?', default='abs', const='north', choices=[
+                        'abs', 'real', 'imag', 'sum'], help='plotting method i.e. real')
+    parser.add_argument('--alpha', '-a', metavar='alpha', type=float,
+                        default=0.0, help='alpha/phi pi fraction')
+    parser.add_argument('--beta', '-b', metavar='beta', type=float,
+                        default=0.0, help='beta/theta pi fraction')
+    args = parser.parse_args()
+
+    return config_dict, args
 
 
-def squashed_gaussian(sig=1):
-    config = setup()
+def grid_fun(theta, phi, theta_0=0, theta_sig=1):
+    return np.exp(-0.5 * ((theta - theta_0) / theta_sig) ** 2) * np.sin(phi)
+
+
+def squashed_gaussian():
+    config, args = setup()
     L = config['L']
     resolution = config['resolution']
-    flm = np.zeros((resolution * resolution), dtype=complex)
 
-    for ell in range(L):
-        for m in range(-ell, ell + 1):
-            ind = ssht.elm2ind(ell, m)
-            flm[ind] = np.exp(m) * np.exp(-ell * (ell + 1)) / (2 * sig * sig)
+    thetas, phis = ssht.sample_positions(resolution, Grid=True)
+    f = grid_fun(thetas, phis)
+    flm = ssht.forward(f, resolution, Reality=True)
+
     return flm
 
 
 if __name__ == '__main__':
-    config = setup()
-    sc = SiftingConvolution(squashed_gaussian, config)
+    config, args = setup()
 
-    # add from command line
-    if config['method'] != 'north':
-        parser = ArgumentParser(description='Create SSHT plot')
-        parser.add_argument('alpha', metavar='alpha',
-                            type=float, help='alpha/phi pi fraction')
-        parser.add_argument('beta', metavar='beta',
-                            type=float, help='beta/theta pi fraction')
-        args = parser.parse_args()
-        alpha_pi_fraction, beta_pi_fraction = args.alpha, args.beta
-        sc.plot(alpha_pi_fraction, beta_pi_fraction)
-    else:
-        sc.plot()
+    if 'method' not in config:
+        config['method'] = args.method
+    if 'plotting_type' not in config:
+        config['plotting_type'] = args.type
+
+    sc = SiftingConvolution(squashed_gaussian, config)
+    sc.plot(args.alpha, args.beta, reality=True)
