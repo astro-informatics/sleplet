0f73f608eb7b4997bc0f394796141a1f3bf8f5b3
paddyroddy <patrickjamesroddy@gmail.com>
2019-08-16 12:06:44 +0100

    add test to confirm morlet convolution is working

--- pys2sleplet_tests.py
+++ pys2sleplet_tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from plotting import Plotting
-from sifting import dirac_delta, earth, identity
+from sifting import dirac_delta, earth, identity, morlet
 from sifting_convolution import SiftingConvolution
 import numpy as np
 import os
@@ -52,8 +52,6 @@ def test_earth_identity_convolution() -> None:
     flm, flm_name, config = earth()
     glm, glm_name, _ = identity()
     sc = SiftingConvolution(flm, flm_name, config, glm, glm_name)
-    sc.calc_nearest_grid_point()
-    plotting = Plotting(auto_open=sc.auto_open, save_fig=sc.save_fig)
 
     # convolution
     flm_conv = sc.convolution(flm, glm)
@@ -62,19 +60,45 @@ def test_earth_identity_convolution() -> None:
     np.testing.assert_equal(flm_conv, flm)
     print("Identity convolution passed test")
 
-    # prepare
+
+def test_earth_morlet_convolution() -> None:
+    # setup
+    flm, flm_name, config = earth()
+    glm, glm_name, _ = morlet()
+    sc = SiftingConvolution(flm, flm_name, config, glm, glm_name)
+    sc.calc_nearest_grid_point()
+    plotting = Plotting(auto_open=sc.auto_open, save_fig=sc.save_fig)
+
+    # map
+    flm_map_boost = plotting.resolution_boost(flm, sc.L, sc.resolution)
+    f_map = ssht.inverse(
+        flm_map_boost, sc.resolution, Reality=sc.reality, Method="MWSS"
+    )
+
+    # convolution
+    flm_conv = sc.convolution(flm, glm)
     flm_conv_boost = plotting.resolution_boost(flm_conv, sc.L, sc.resolution)
     f_conv = ssht.inverse(
         flm_conv_boost, sc.resolution, Reality=sc.reality, Method="MWSS"
     )
 
+    # calculate difference
+    flm_diff = flm - flm_conv
+    f_diff = f_map - f_conv
+
+    # perform test
+    np.testing.assert_allclose(flm, flm_conv, atol=0.5)
+    np.testing.assert_allclose(f_map, f_conv, atol=500)
+    print("Earth/morlet convolution difference max error:", np.max(np.abs(flm_diff)))
+
     # filename
-    filename = f"identity_L-{sc.L}_convolved_earth_L-{sc.L}_res-{sc.resolution}_real"
+    filename = f"earth_L-{sc.L}_diff_morlet_res-{sc.resolution}_real"
 
     # create plot
-    plotting.plotly_plot(f_conv.real, sc.resolution, filename, sc.annotations())
+    plotting.plotly_plot(f_diff.real, sc.resolution, filename, sc.annotations())
 
 
 if __name__ == "__main__":
     test_dirac_delta_rotate_translate()
     test_earth_identity_convolution()
+    test_earth_morlet_convolution()
