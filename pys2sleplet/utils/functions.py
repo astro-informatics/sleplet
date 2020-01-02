448e2331d8f32f94b7bb3ff12654a8faad0bed1a
paddyroddy <patrickjamesroddy@gmail.com>
2020-01-02 22:49:44 +0000

    ran pre-commit on all files

--- pys2sleplet/utils/functions.py
+++ pys2sleplet/utils/functions.py
@@ -3,11 +3,12 @@ from pys2sleplet.flm.kernels.elongated_gaussian import ElongatedGaussian
 from pys2sleplet.flm.kernels.gaussian import Gaussian
 from pys2sleplet.flm.kernels.harmonic_gaussian import HarmonicGaussian
 from pys2sleplet.flm.kernels.identity import Identity
-from pys2sleplet.flm.kernels.slepian import Slepian
 from pys2sleplet.flm.kernels.spherical_harmonic import SphericalHarmonic
 from pys2sleplet.flm.kernels.squashed_gaussian import SquashedGaussian
 from pys2sleplet.flm.maps.earth import Earth
 from pys2sleplet.flm.maps.wmap import WMAP
+
+# from pys2sleplet.flm.kernels.slepian import Slepian
 
 function_dict = {
     "dirac_delta": DiracDelta,
@@ -16,7 +17,7 @@ function_dict = {
     "gaussian": Gaussian,
     "harmonic_gaussian": HarmonicGaussian,
     "identity": Identity,
-    "slepian": Slepian,
+    # "slepian": Slepian,
     "spherical_harmonic": SphericalHarmonic,
     "squashed_gaussian": SquashedGaussian,
     "wmap": WMAP,
