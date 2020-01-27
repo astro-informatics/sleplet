7b4213fcc1bc80ec9daf1b3717ccd624356c864c
paddyroddy <patrickjamesroddy@gmail.com>
2020-01-27 22:33:34 +0000

    use console entry points

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1.0",
     author="Patrick Roddy",
     author_email="patrickjamesroddy@gmail.com",
-    packages=find_packages(),
-    scripts=["scripts/plotting.py", "scripts/convert_data.py"],
+    packages=find_packages(exclude=["*test"]),
+    include_package_data=True,
+    entry_points={"console_scripts": ["plotting=pys2sleplet.scripts.command:main"]},
 )
