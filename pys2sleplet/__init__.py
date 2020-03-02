c2b223b61a6f70edadb1f33f4d150f23004c7247
paddyroddy <patrickjamesroddy@gmail.com>
2020-03-02 11:04:18 +0000

    get rid of __init__.py files in package

--- pys2sleplet/__init__.py
+++
@@ -1,4 +0,0 @@
-import pys2sleplet.flm
-import pys2sleplet.plotting
-import pys2sleplet.slepian
-import pys2sleplet.utils
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 
 setup(
     name="pys2sleplet",
     version="0.1.0",
     author="Patrick Roddy",
     author_email="patrickjamesroddy@gmail.com",
-    packages=find_packages(exclude=["*test"]),
+    packages=find_namespace_packages(),
     include_package_data=True,
     entry_points={"console_scripts": ["plotting=pys2sleplet.scripts.plotting:main"]},
 )
