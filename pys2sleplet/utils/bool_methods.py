66ea9aaff46c1aa596712d98bbb5b11209c4be2a
paddyroddy <patrickjamesroddy@gmail.com>
2020-05-18 10:18:49 +0100

    remove else in bool methods

--- pys2sleplet/utils/bool_methods.py
+++ pys2sleplet/utils/bool_methods.py
@@ -15,15 +15,13 @@ def is_polar_cap(
     """
     circular mask at the north pole
     """
-    if (
+    condition = bool(
         phi_min == np.deg2rad(PHI_MIN_DEFAULT)
         and phi_max == np.deg2rad(PHI_MAX_DEFAULT)
         and theta_min == np.deg2rad(THETA_MIN_DEFAULT)
         and theta_max != np.deg2rad(THETA_MAX_DEFAULT)
-    ):
-        return True
-    else:
-        return False
+    )
+    return condition
 
 
 def is_limited_lat_lon(
@@ -32,33 +30,27 @@ def is_limited_lat_lon(
     """
     a region defined by angles, just need one to not be the default
     """
-    if (
+    condition = bool(
         not is_polar_cap(phi_min, phi_max, theta_min, theta_max)
         and phi_min != np.deg2rad(PHI_MIN_DEFAULT)
         or phi_max != np.deg2rad(PHI_MAX_DEFAULT)
         or theta_min != np.deg2rad(THETA_MIN_DEFAULT)
         or theta_max != np.deg2rad(THETA_MAX_DEFAULT)
-    ):
-        return True
-    else:
-        return False
+    )
+    return condition
 
 
 def is_small_polar_cap(theta_max: float) -> bool:
     """
     assuming it is a polar cap small defined for visualisation purposes
     """
-    if theta_max <= np.pi / 4:
-        return True
-    else:
-        return False
+    condition = bool(theta_max <= np.pi / 4)
+    return condition
 
 
 def is_polar_gap() -> bool:
     """
     assuming it is a polar gap
     """
-    if config.POLAR_GAP:
-        return True
-    else:
-        return False
+    condition = bool(config.POLAR_GAP)
+    return condition
