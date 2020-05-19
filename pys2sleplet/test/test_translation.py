9029339b21e596b5dc90de97cf63f742979f6658
paddyroddy <patrickjamesroddy@gmail.com>
2020-05-19 18:08:12 +0100

    test multiple angles for rotation/translation using hypothesis

--- pys2sleplet/test/test_translation.py
+++ pys2sleplet/test/test_translation.py
@@ -1,5 +1,6 @@
 import numpy as np
-import pytest
+from hypothesis import given, settings
+from hypothesis.strategies import SearchStrategy, floats
 
 from pys2sleplet.flm.kernels.dirac_delta import DiracDelta
 from pys2sleplet.plotting.create_plot import Plot
@@ -7,30 +8,36 @@ from pys2sleplet.utils.config import config
 from pys2sleplet.utils.logger import logger
 
 
-@pytest.fixture
-def alpha_pi_frac() -> float:
-    return 0.75
+def alpha_pi_frac() -> SearchStrategy[float]:
+    """
+    alpha can be in the range [0, 2*pi)
+    """
+    return floats(min_value=0, max_value=2, exclude_max=True, width=16)
 
 
-@pytest.fixture
-def beta_pi_frac() -> float:
-    return 0.125
+def beta_pi_frac() -> SearchStrategy[float]:
+    """
+    beta can be in the range [0, pi]
+    """
+    return floats(min_value=0, max_value=1, width=16)
 
 
-def test_dirac_delta_rotate_translate(alpha_pi_frac, beta_pi_frac) -> None:
+@settings(max_examples=8, derandomize=True, deadline=None)
+@given(alpha=alpha_pi_frac(), beta=beta_pi_frac())
+def test_dirac_delta_rotate_translate(alpha, beta) -> None:
     """
     test to ensure that rotation and translation
     give the same result for the Dirac delta
     """
     # rotation
     dd = DiracDelta(config.L)
-    dd.rotate(alpha_pi_frac, beta_pi_frac)
+    dd.rotate(alpha, beta)
     flm_rot = dd.multipole
     f_rot, f_rot_plot = dd.field, dd.plot
 
     # translation
     dd = DiracDelta(config.L)
-    dd.translate(alpha_pi_frac, beta_pi_frac)
+    dd.translate(alpha, beta)
     flm_trans = dd.multipole
     f_trans, f_trans_plot = dd.field, dd.plot
 
@@ -39,8 +46,8 @@ def test_dirac_delta_rotate_translate(alpha_pi_frac, beta_pi_frac) -> None:
     f_diff = f_rot_plot - f_trans_plot
 
     # perform test
-    np.testing.assert_allclose(flm_rot, flm_trans, rtol=1e-14)
-    np.testing.assert_allclose(f_rot, f_trans, rtol=1e-12)
+    np.testing.assert_allclose(flm_rot, flm_trans, rtol=1e-13)
+    np.testing.assert_allclose(f_rot, f_trans, rtol=1e-11)
     logger.info(f"Translation/rotation difference max error: {np.abs(flm_diff).max()}")
 
     if config.AUTO_OPEN:
