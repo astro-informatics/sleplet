aeb351d56d1f66271682c844384b7551151d8ba2
paddyroddy <patrickjamesroddy@gmail.com>
2020-05-19 19:28:24 +0100

    failing test on rot/trans

--- pys2sleplet/test/test_translation.py
+++ pys2sleplet/test/test_translation.py
@@ -1,11 +1,12 @@
 import numpy as np
 from hypothesis import given, settings
 from hypothesis.strategies import SearchStrategy, floats
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_raises
 
 from pys2sleplet.flm.kernels.dirac_delta import DiracDelta
 from pys2sleplet.plotting.create_plot import Plot
 from pys2sleplet.utils.config import config
+from pys2sleplet.utils.dicts import kernels
 from pys2sleplet.utils.logger import logger
 
 
@@ -46,3 +47,24 @@ def test_dirac_delta_rotate_translate(alpha_pi_frac, beta_pi_frac) -> None:
     if config.AUTO_OPEN:
         filename = f"{dd_1.name}_L{config.L}_diff_rot_trans_res{dd_1.resolution}"
         Plot(f_diff.real, dd_1.resolution, filename).execute()
+
+
+@settings(max_examples=1, derandomize=True, deadline=None)
+@given(alpha_pi_frac=valid_alphas(), beta_pi_frac=valid_betas())
+def test_other_kernels_rotate_translate(alpha_pi_frac, beta_pi_frac) -> None:
+    """
+    test to ensure that rotation and translation
+    give the same result for the Dirac delta
+    """
+    for name, kernel in kernels.items():
+        if name in {"dirac_delta", "slepian"}:
+            continue
+
+        k_1 = kernel(config.L)
+        k_1.rotate(alpha_pi_frac, beta_pi_frac)
+
+        k_2 = kernel(config.L)
+        k_2.translate(alpha_pi_frac, beta_pi_frac)
+
+        assert_raises(AssertionError, assert_allclose, k_1.multipole, k_2.multipole)
+        assert_raises(AssertionError, assert_allclose, k_1.field, k_2.field)
