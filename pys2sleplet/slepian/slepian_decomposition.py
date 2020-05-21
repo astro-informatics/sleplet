6a53b79479b78afdf0827a4509482403d44c49b6
paddyroddy <patrickjamesroddy@gmail.com>
2020-05-21 15:21:08 +0100

    prepare Slepian decomposition dataclass

--- pys2sleplet/slepian/slepian_decomposition.py
+++ pys2sleplet/slepian/slepian_decomposition.py
@@ -1,42 +1,47 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 import numpy as np
 import pyssht as ssht
 
 from pys2sleplet.flm.functions import Functions
-from pys2sleplet.utils.slepian_methods import choose_slepian_method
+from pys2sleplet.slepian.slepian_functions import SlepianFunctions
+from pys2sleplet.utils.vars import SAMPLING_SCHEME
 
 
 @dataclass
 class SlepianDecomposition:
     L: int
     function: Functions
-    flm: np.ndarray
-    f: np.ndarray
-    lambdas: np.ndarray
-    s: np.ndarray
-    thetas: np.ndarray
-    delta_phi: float
-    delta_theta: float
+    slepian: SlepianFunctions
+    flm: np.ndarray = field(init=False, repr=False)
+    f: np.ndarray = field(init=False, repr=False)
+    lambdas: np.ndarray = field(init=False, repr=False)
+    s: np.ndarray = field(init=False, repr=False)
+    theta_grid: np.ndarray = field(init=False, repr=False)
+    weight: np.ndarray = field(init=False, repr=False)
 
     def __post_init__(self) -> None:
-        slepian = choose_slepian_method(self.L)
-        self.lambdas = slepian.slepian_evals
-        self.s = slepian.slepian_evecs
-        self.thetas, phis = ssht.sample_positions(self.L, Method="MWSS")
-        self.delta_phi = np.ediff1d(phis[0]).mean()
-        self.delta_theta = np.ediff1d(self.thetas[:, 0]).mean()
+        self.flm = self.function.multipole
+        self.f = np.where(self.slepian.mask, self.function.field, 0)
+        self.lambdas = self.slepian.eigenvalues
+        self.s = self.slepian.eigenvectors
+        self.theta_grid, phi_grid = ssht.sample_positions(
+            self.L, Grid=True, Method=SAMPLING_SCHEME
+        )
+        delta_theta = np.ediff1d(self.theta_grid[:, 0]).mean()
+        delta_phi = np.ediff1d(phi_grid[0]).mean()
+        self.weight = self.theta_grid * delta_theta * delta_phi
 
-    def decompose(self, rank: int, method: str = "harmonic_sum") -> np.ndarray:
+    def decompose(self, rank: int, method: str) -> np.ndarray:
         """
         decompose the signal into its Slepian coefficients via the given method
         """
         if method == "integrate_region":
             f_p = self._integrate_region(rank)
+        elif method == "integrate_sphere":
+            f_p = self._integrate_sphere(rank)
         elif method == "forward_transform":
             f_p = self._forward_transform(rank)
-        elif method == "harmonic_sum":
-            f_p = self._harmonic_sum(rank)
         else:
             raise ValueError(
                 f"{method} is not a recognised Slepian decomposition method"
@@ -50,27 +55,23 @@ class SlepianDecomposition:
         \int\limits_{R} \dd{\Omega(\omega)}
         f(\omega) \overline{S_{p}(\omega)}
         """
+        region = self.slepian.mask
         integrand = self.f * self.s[rank].conj()
-
-        weight = np.sin(self.thetas) * self.delta_theta * self.delta_phi
-
-        f_p = (integrand * weight).sum() / self.lambdas[rank]
+        integral = (integrand * self.weight)[region].sum()
+        f_p = integral / self.lambdas[rank]
         return f_p
 
-    def _forward_transform(self, rank: int) -> np.ndarray:
+    def _integrate_sphere(self, rank: int) -> np.ndarray:
         """
         f_{p} =
         \int\limits_{S^{2}} \dd{\Omega(\omega)}
         f(\omega) \overline{S_{p}(\omega)}
         """
         integrand = self.f * self.s[rank].conj()
-
-        weight = np.sin(self.thetas) * self.delta_theta * self.delta_phi
-
-        f_p = (integrand * weight).sum()
+        f_p = (integrand * self.weight).sum()
         return f_p
 
-    def _harmonic_sum(self, rank: int) -> np.ndarray:
+    def _forward_transform(self, rank: int) -> np.ndarray:
         """
         f_{p} =
         \sum\limits_{\ell=0}^{L^{2}}
@@ -79,10 +80,9 @@ class SlepianDecomposition:
         \int\limits_{S^{2}} \dd{\Omega(\omega)}
         Y_{\ell m}(\omega) \overline{S_{p}(\omega)}
         """
-        s_p_lm = ssht.forward(self.s[rank], self.L, Method="MWSS")
-
+        s_p_lm = ssht.forward(self.s[rank], self.L, Method=SAMPLING_SCHEME)
         summation = self.flm * s_p_lm.conj()
 
-        # same as double sum with index
+        # equivalent to looping through l and m
         f_p = summation.sum()
         return f_p
