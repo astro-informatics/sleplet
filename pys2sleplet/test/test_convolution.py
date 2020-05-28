45fab8f0f0d7ee0ff7fbb233237ae93f9c3d2ed0
paddyroddy <patrickjamesroddy@gmail.com>
2020-05-28 16:57:29 +0100

    fix L within tests not config file

--- pys2sleplet/test/test_convolution.py
+++ pys2sleplet/test/test_convolution.py
@@ -1,20 +1,28 @@
 import numpy as np
+import pytest
 from numpy.testing import assert_allclose, assert_array_equal
 
 from pys2sleplet.flm.kernels.harmonic_gaussian import HarmonicGaussian
 from pys2sleplet.flm.kernels.identity import Identity
 from pys2sleplet.flm.maps.earth import Earth
-from pys2sleplet.utils.config import config
 from pys2sleplet.utils.logger import logger
 
 
-def test_earth_identity_convolution() -> None:
+@pytest.fixture
+def L() -> int:
+    """
+    needs to fixed for closeness tests
+    """
+    return 32
+
+
+def test_earth_identity_convolution(L) -> None:
     """
     test to ensure that the convolving with the
     identity function doesn't change the map
     """
-    f = Earth(config.L)
-    g = Identity(config.L)
+    f = Earth(L)
+    g = Identity(L)
     flm = f.multipole
 
     f.convolve(g.multipole)
@@ -24,13 +32,13 @@ def test_earth_identity_convolution() -> None:
     logger.info("Identity convolution passed test")
 
 
-def test_earth_harmonic_gaussian_convolution() -> None:
+def test_earth_harmonic_gaussian_convolution(L) -> None:
     """
     test to ensure that convolving the Earth with the harmonic
     Gausian does not change significantly change the map
     """
-    f = Earth(config.L)
-    g = HarmonicGaussian(config.L)
+    f = Earth(L)
+    g = HarmonicGaussian(L)
     flm, f_map = f.multipole, f.field
 
     f.convolve(g.multipole)
--- pys2sleplet/test/test_translation.py
+++ pys2sleplet/test/test_translation.py
@@ -1,11 +1,19 @@
 import numpy as np
+import pytest
 from hypothesis import given, settings
 from hypothesis.strategies import SearchStrategy, floats
 from numpy.testing import assert_allclose
 
 from pys2sleplet.flm.kernels.dirac_delta import DiracDelta
-from pys2sleplet.utils.config import config
 from pys2sleplet.utils.logger import logger
+
+
+@pytest.fixture
+def L() -> int:
+    """
+    needs to fixed for closeness tests
+    """
+    return 32
 
 
 def valid_alphas() -> SearchStrategy[float]:
@@ -24,15 +32,15 @@ def valid_betas() -> SearchStrategy[float]:
 
 @settings(max_examples=8, derandomize=True, deadline=None)
 @given(alpha_pi_frac=valid_alphas(), beta_pi_frac=valid_betas())
-def test_dirac_delta_rotate_translate(alpha_pi_frac, beta_pi_frac) -> None:
+def test_dirac_delta_rotate_translate(L, alpha_pi_frac, beta_pi_frac) -> None:
     """
     test to ensure that rotation and translation
     give the same result for the Dirac delta
     """
-    dd_1 = DiracDelta(config.L)
+    dd_1 = DiracDelta(L)
     dd_1.rotate(alpha_pi_frac, beta_pi_frac)
 
-    dd_2 = DiracDelta(config.L)
+    dd_2 = DiracDelta(L)
     dd_2.translate(alpha_pi_frac, beta_pi_frac)
 
     flm_diff = dd_1.multipole - dd_2.multipole
