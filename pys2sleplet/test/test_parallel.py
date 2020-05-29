61b9c87ad0463139fececefafc5aa61c99ccf341
paddyroddy <patrickjamesroddy@gmail.com>
2020-05-29 14:22:00 +0100

    limit lat long parallel test

--- pys2sleplet/test/test_parallel.py
+++ pys2sleplet/test/test_parallel.py
@@ -4,8 +4,10 @@ from hypothesis import given, settings
 from hypothesis.strategies import SearchStrategy, integers
 from numpy.testing import assert_array_equal
 
+from pys2sleplet.slepian.slepian_region.slepian_limit_lat_long import (
+    SlepianLimitLatLong,
+)
 from pys2sleplet.slepian.slepian_region.slepian_polar_cap import SlepianPolarCap
-from pys2sleplet.utils.config import config
 
 
 @pytest.fixture(scope="module")
@@ -16,12 +18,32 @@ def L() -> int:
     return 8
 
 
+def valid_theta_min() -> SearchStrategy[int]:
+    """
+    theta can be in the range [0, 180]
+    """
+    return integers(min_value=1, max_value=30)
+
+
 def valid_theta_max() -> SearchStrategy[int]:
     """
     theta can be in the range [0, 180]
-    however we will restrict theta_max to 60 so it is a cap
     """
-    return integers(min_value=1, max_value=60)
+    return integers(min_value=31, max_value=60)
+
+
+def valid_phi_min() -> SearchStrategy[int]:
+    """
+    phi can be in the range [0, 360)
+    """
+    return integers(min_value=1, max_value=30)
+
+
+def valid_phi_max() -> SearchStrategy[int]:
+    """
+    phi can be in the range [0, 360)
+    """
+    return integers(min_value=31, max_value=60)
 
 
 def valid_orders() -> SearchStrategy[int]:
@@ -38,16 +60,40 @@ def test_slepian_polar_cap_serial_equal_to_parallel(L, theta_max, order) -> None
     ensures that the serial and parallel calculation of a given
     Slepian polar cap give the same result
     """
-    t_max = np.deg2rad(theta_max)
-    serial = SlepianPolarCap(L, t_max, order=order, ncpu=1)
-    parallel = SlepianPolarCap(L, t_max, order=order, ncpu=config.NCPU)
+    serial = SlepianPolarCap(L, np.deg2rad(theta_max), order=order, ncpu=1)
+    parallel = SlepianPolarCap(L, np.deg2rad(theta_max), order=order)
     assert_array_equal(serial.eigenvalues, parallel.eigenvalues)
     assert_array_equal(serial.eigenvectors, parallel.eigenvectors)
 
 
-def test_slepian_lat_lon_serial_equal_to_parallel() -> None:
+@settings(max_examples=8, derandomize=True, deadline=None)
+@given(
+    theta_min=valid_theta_min(),
+    theta_max=valid_theta_max(),
+    phi_min=valid_phi_min(),
+    phi_max=valid_phi_max(),
+)
+def test_slepian_lat_lon_serial_equal_to_parallel(
+    L, theta_min, theta_max, phi_min, phi_max
+) -> None:
     """
     ensures that the serial and parallel calculation of a given
     Slepian limited latitude longitude region give the same result
     """
-    pass
+    serial = SlepianLimitLatLong(
+        L,
+        np.deg2rad(theta_min),
+        np.deg2rad(theta_max),
+        np.deg2rad(phi_min),
+        np.deg2rad(phi_max),
+        ncpu=1,
+    )
+    parallel = SlepianLimitLatLong(
+        L,
+        np.deg2rad(theta_min),
+        np.deg2rad(theta_max),
+        np.deg2rad(phi_min),
+        np.deg2rad(phi_max),
+    )
+    assert_array_equal(serial.eigenvalues, parallel.eigenvalues)
+    assert_array_equal(serial.eigenvectors, parallel.eigenvectors)
