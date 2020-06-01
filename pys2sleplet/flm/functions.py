e0a6236c319bf5c2fa12407939d2db1efc565a1d
paddyroddy <patrickjamesroddy@gmail.com>
2020-06-01 15:53:21 +0100

    fix convolution display

--- pys2sleplet/flm/functions.py
+++ pys2sleplet/flm/functions.py
@@ -101,7 +101,7 @@ class Functions:
         # function so turn off reality except for Dirac delta
         self.reality = False
 
-        self.multipole *= glm.conj()
+        self.multipole *= glm
 
     @property
     def annotations(self) -> List[Dict]:
