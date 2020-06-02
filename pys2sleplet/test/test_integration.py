092746970e0425bc7f877f9ba9f49aff442bf46d
paddyroddy <patrickjamesroddy@gmail.com>
2020-06-02 16:00:49 +0100

    create low accuracy integration tests

--- pys2sleplet/test/test_integration.py
+++ pys2sleplet/test/test_integration.py
@@ -15,7 +15,11 @@ from pys2sleplet.test.constants import (
     THETA_1,
     THETA_MAX,
 )
-from pys2sleplet.utils.integration_methods import integrate_whole_sphere
+from pys2sleplet.utils.integration_methods import (
+    integrate_region_sphere,
+    integrate_whole_sphere,
+)
+from pys2sleplet.utils.region import Region
 
 
 @pytest.fixture(scope="module")
@@ -24,6 +28,14 @@ def slepian_polar_cap() -> SlepianPolarCap:
     retrieve the eigenvectors of a Slepian polar cap
     """
     return SlepianPolarCap(L, THETA_MAX, order=ORDER)
+
+
+@pytest.fixture(scope="module")
+def polar_cap_region() -> Region:
+    """
+    creates a polar cap region
+    """
+    return Region(theta_max=THETA_1)
 
 
 def valid_polar_ranks() -> SearchStrategy[int]:
@@ -41,6 +53,14 @@ def slepian_lim_lat_lon() -> SlepianLimitLatLon:
     return SlepianLimitLatLon(
         L, theta_min=THETA_0, theta_max=THETA_1, phi_min=PHI_0, phi_max=PHI_1
     )
+
+
+@pytest.fixture(scope="module")
+def lim_lat_lon_region() -> Region:
+    """
+    creates a limited latitude longitude region
+    """
+    return Region(theta_min=THETA_0, theta_max=THETA_1, phi_min=PHI_0, phi_max=PHI_1)
 
 
 def valid_lim_lat_lon_ranks() -> SearchStrategy[int]:
@@ -86,3 +106,43 @@ def test_integrate_two_slepian_lim_lat_lon_functions_whole_sphere(
         assert_allclose(output, 1, rtol=1e-5)
     else:
         assert_allclose(output, 0, atol=1e-5)
+
+
+@seed(RANDOM_SEED)
+@settings(max_examples=8, deadline=None)
+@given(rank1=valid_polar_ranks(), rank2=valid_polar_ranks())
+def test_integrate_two_slepian_polar_functions_region_sphere(
+    slepian_polar_cap, polar_cap_region, rank1, rank2
+) -> None:
+    """
+    tests that integration two slepian functions over a region on
+    the sphere gives the Kronecker delta multiplied by the eigenvalue
+    """
+    lambda_p = slepian_polar_cap.eigenvalues[rank1]
+    flm = slepian_polar_cap.eigenvectors[rank1]
+    glm = slepian_polar_cap.eigenvectors[rank2]
+    output = integrate_region_sphere(L, flm, glm, polar_cap_region, glm_conj=True)
+    if rank1 == rank2:
+        assert_allclose(output, lambda_p, rtol=0.3)
+    else:
+        assert_allclose(output, 0, atol=0.2)
+
+
+@seed(RANDOM_SEED)
+@settings(max_examples=8, deadline=None)
+@given(rank1=valid_lim_lat_lon_ranks(), rank2=valid_lim_lat_lon_ranks())
+def test_integrate_two_slepian_lim_lat_lon_functions_region_sphere(
+    slepian_lim_lat_lon, lim_lat_lon_region, rank1, rank2
+) -> None:
+    """
+    tests that integration two slepian functions over a region on
+    the sphere gives the Kronecker delta multiplied by the eigenvalue
+    """
+    lambda_p = slepian_lim_lat_lon.eigenvalues[rank1]
+    flm = slepian_lim_lat_lon.eigenvectors[rank1]
+    glm = slepian_lim_lat_lon.eigenvectors[rank2]
+    output = integrate_whole_sphere(L, flm, glm, lim_lat_lon_region, glm_conj=True)
+    if rank1 == rank2:
+        assert_allclose(output, lambda_p, rtol=0.7)
+    else:
+        assert_allclose(output, 0, atol=0.3)
