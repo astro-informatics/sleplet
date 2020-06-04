cb56da651632535318386076cf386d4d0f6bcc10
paddyroddy <patrickjamesroddy@gmail.com>
2020-06-04 18:23:02 +0100

    tests for all decomposition

--- pys2sleplet/test/test_decomposition.py
+++ pys2sleplet/test/test_decomposition.py
@@ -133,6 +133,32 @@ def test_decompose_region_and_integrate_sphere_agree_lim_lat_lon(
     assert_allclose(f_p, g_p, rtol=49.8)
 
 
+def test_decompose_all_polar(polar_cap_decomposition) -> None:
+    """
+    tests that all three methods produce the same coefficients for polar cap
+    """
+    f_p = polar_cap_decomposition.decompose_all(method="integrate_region")
+    g_p = polar_cap_decomposition.decompose_all(method="integrate_sphere")
+    h_p = polar_cap_decomposition.decompose_all(method="harmonic_sum")
+    assert_allclose(f_p, g_p, rtol=1e8)
+    assert_allclose(g_p, h_p, rtol=1e-3)
+    assert_allclose(h_p, f_p, rtol=1.1)
+
+
+@pytest.mark.slow
+def test_decompose_all_lim_lat_lon(lim_lat_lon_decomposition) -> None:
+    """
+    tests that all three methods produce the same coefficients for
+    limited latitude longitude region
+    """
+    f_p = lim_lat_lon_decomposition.decompose_all(method="integrate_region")
+    g_p = lim_lat_lon_decomposition.decompose_all(method="integrate_sphere")
+    h_p = lim_lat_lon_decomposition.decompose_all(method="harmonic_sum")
+    assert_allclose(f_p, g_p, rtol=1e10)
+    assert_allclose(g_p, h_p, rtol=0.3)
+    assert_allclose(h_p, f_p, rtol=2)
+
+
 def test_pass_function_without_region() -> None:
     """
     tests that the class throws an exception if no Region is passed to the function
