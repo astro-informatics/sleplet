da1b59e0744cb947fba040204bea98e9a881ff73
paddyroddy <patrickjamesroddy@gmail.com>
2020-06-05 19:39:41 +0100

    conjugate sign

--- pys2sleplet/flm/functions.py
+++ pys2sleplet/flm/functions.py
@@ -106,7 +106,7 @@ class Functions:
         # function so turn off reality except for Dirac delta
         self.reality = False
 
-        multipole = flm * glm
+        multipole = flm * glm.conj()
         return multipole
 
     @property
