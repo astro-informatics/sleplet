9e7a7780cdfa1164cea9567a3605e9723b1aee74
paddyroddy <patrickjamesroddy@gmail.com>
2020-06-09 14:11:38 +0100

    create explicit list of tuples of order and rank

--- pys2sleplet/scripts/simons_polar_cap/fried_egg.py
+++ pys2sleplet/scripts/simons_polar_cap/fried_egg.py
@@ -1,6 +1,6 @@
 from pys2sleplet.flm.kernels.slepian import Slepian
 from pys2sleplet.scripts.plotting import plot
-from pys2sleplet.scripts.simons_polar_cap.simons_inputs import ORDER_DICT, THETA_MAX, L
+from pys2sleplet.scripts.simons_polar_cap.simons_inputs import ORDER_RANK, THETA_MAX, L
 from pys2sleplet.utils.logger import logger
 from pys2sleplet.utils.region import Region
 
@@ -9,18 +9,19 @@ def produce_figure() -> None:
     """
     create fig 5.4 from Spatiospectral Concentration on a Sphere by Simons et al 2006
     """
-    for order, rank in ORDER_DICT.items():
+    for order, rank in ORDER_RANK:
         _helper(order, rank)
-        if order != 0:
-            _helper(-order, rank)
 
 
 def _helper(order: int, rank: int) -> None:
     """
     helper which plots the required order and specified ranks
     """
+    logger.info(f"plotting order={order}, rank={rank}")
     region = Region(theta_max=THETA_MAX, order=order)
-    for r in range(rank):
-        logger.info(f"plotting order={order}, rank={r}")
-        slepian = Slepian(L, rank=r, region=region)
-        plot(slepian)
+    slepian = Slepian(L, rank=rank, region=region)
+    plot(slepian)
+
+
+if __name__ == "__main__":
+    produce_figure()
