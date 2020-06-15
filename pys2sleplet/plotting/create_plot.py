af035de9589b415ef839bde57285454ba802e880
paddyroddy <patrickjamesroddy@gmail.com>
2020-06-15 15:39:31 +0100

    open plot before saving it

--- pys2sleplet/plotting/create_plot.py
+++ pys2sleplet/plotting/create_plot.py
@@ -89,17 +89,17 @@ class Plot:
 
         fig = Figure(data=data, layout=layout)
 
+        # create html and open if auto_open is true
+        html_filename = str(_fig_path / "html" / f"{self.filename}.html")
+
+        py.plot(fig, filename=html_filename, auto_open=config.AUTO_OPEN)
+
         # if save_fig is true then create png and pdf in their directories
         if config.SAVE_FIG:
             for file_type in ["png", "pdf"]:
                 logger.info(f"saving {file_type}")
                 filename = str(_fig_path / file_type / f"{self.filename}.{file_type}")
                 pio.write_image(fig, filename)
-
-        # create html and open if auto_open is true
-        html_filename = str(_fig_path / "html" / f"{self.filename}.html")
-
-        py.plot(fig, filename=html_filename, auto_open=config.AUTO_OPEN)
 
     @staticmethod
     def _setup_plot(
