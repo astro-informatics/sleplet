a9ed2e0886cfd6b61db26feb7cdcb74b31e88c86
paddyroddy <patrickjamesroddy@gmail.com>
2020-06-15 16:20:27 +0100

    white textbox

--- pys2sleplet/plotting/polar_cap/inputs.py
+++ pys2sleplet/plotting/polar_cap/inputs.py
@@ -1,7 +1,5 @@
 from typing import Dict, Union
 
 L: int = 19
-TEXT_BOX: Dict[str, Union[str, float]] = dict(
-    boxstyle="round", facecolor="wheat", alpha=0.5
-)
+TEXT_BOX: Dict[str, Union[str, float]] = dict(boxstyle="round")
 THETA_MAX: int = 40
