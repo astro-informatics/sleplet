9d2c4e4482b193e8df73b05f7367652dddb530a0
paddyroddy <patrickjamesroddy@gmail.com>
2020-06-30 15:14:05 +0100

    neaten up config file up a bit

--- pys2sleplet/utils/config.py
+++ pys2sleplet/utils/config.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from dynaconf import Dynaconf
 
 _file_location = Path(__file__).resolve()
+_settings_file = _file_location.parents[1] / "config" / "settings.toml"
 
-settings = Dynaconf(
-    settings_files=[_file_location.parents[1] / "config" / "settings.toml"]
-)
+settings = Dynaconf(settings_files=[_settings_file])
