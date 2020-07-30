3e1013bb7126302db4e10d63de44598a60ed7835
paddyroddy <patrickjamesroddy@gmail.com>
2020-07-30 17:38:08 +0100

    pys2let lives at top folder

--- pys2sleplet/utils/pys2let.py
+++ pys2sleplet/utils/pys2let.py
@@ -1,6 +1,6 @@
 import os
 import sys
 
-sys.path.append(os.path.join(os.environ["S2LET"], "src", "main", "python"))
+sys.path.append(os.environ["S2LET"])
 
 import pys2let as s2let  # noqa:E402,F401 isort:skip
