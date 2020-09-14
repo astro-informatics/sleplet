0b8d7ce0f6d76bbd665cdf5ba5691d7745faaf6a
paddyroddy <patrickjamesroddy@gmail.com>
2020-09-14 17:00:23 +0100

    Create wavelet helper class

--- pys2sleplet/utils/function_dicts.py
+++ pys2sleplet/utils/function_dicts.py
@@ -1,18 +1,20 @@
 from typing import Dict
 
 from pys2sleplet.flm.functions import Functions
-from pys2sleplet.flm.kernels.axisymmetric_wavelets import AxisymmetricWavelets
 from pys2sleplet.flm.kernels.dirac_delta import DiracDelta
-from pys2sleplet.flm.kernels.directional_spin_wavelets import DirectionalSpinWavelets
 from pys2sleplet.flm.kernels.elongated_gaussian import ElongatedGaussian
 from pys2sleplet.flm.kernels.gaussian import Gaussian
 from pys2sleplet.flm.kernels.harmonic_gaussian import HarmonicGaussian
 from pys2sleplet.flm.kernels.identity import Identity
-from pys2sleplet.flm.kernels.ridgelets import Ridgelets
 from pys2sleplet.flm.kernels.slepian import Slepian
-from pys2sleplet.flm.kernels.slepian_wavelets import SlepianWavelets
 from pys2sleplet.flm.kernels.spherical_harmonic import SphericalHarmonic
 from pys2sleplet.flm.kernels.squashed_gaussian import SquashedGaussian
+from pys2sleplet.flm.kernels.wavelets.axisymmetric_wavelets import AxisymmetricWavelets
+from pys2sleplet.flm.kernels.wavelets.directional_spin_wavelets import (
+    DirectionalSpinWavelets,
+)
+from pys2sleplet.flm.kernels.wavelets.ridgelets import Ridgelets
+from pys2sleplet.flm.kernels.wavelets.slepian_wavelets import SlepianWavelets
 from pys2sleplet.flm.maps.earth import Earth
 from pys2sleplet.flm.maps.south_america import SouthAmerica
 from pys2sleplet.flm.maps.wmap import Wmap
