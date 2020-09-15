0616d8cf8d3e1d944ffb0dafbee53310d1543810
paddyroddy <patrickjamesroddy@gmail.com>
2020-09-15 11:23:18 +0100

    Create thresholding helper methods

--- pys2sleplet/plotting/misc/denoising_axisym.py
+++ pys2sleplet/plotting/misc/denoising_axisym.py
@@ -4,14 +4,12 @@ import pyssht as ssht
 from pys2sleplet.flm.kernels.axisymmetric_wavelets import AxisymmetricWavelets
 from pys2sleplet.flm.maps.earth import Earth
 from pys2sleplet.plotting.create_plot import Plot
-from pys2sleplet.utils.noise import compute_snr, signal_power
+from pys2sleplet.utils.noise import compute_sigma_j, compute_snr, hard_thresholding
 from pys2sleplet.utils.plot_methods import calc_plot_resolution
 
 B = 2
 J_MIN = 0
 L = 128
-N_SIGMA = 3
-SNR_IN = 10
 
 
 def main() -> None:
@@ -22,11 +20,11 @@ def main() -> None:
     earth = Earth(L)
     earth_noised = Earth(L, noise=True)
 
-    # compute noise sigma
-    sigma_noise = np.sqrt(10 ** (-SNR_IN / 10) * signal_power(L, earth.multipole))
-
     # create wavelets
     aw = AxisymmetricWavelets(L, B=B, j_min=J_MIN)
+
+    # compute wavelet noise
+    sigma_j = compute_sigma_j(L, earth.multipole, aw.wavelets[1:])
 
     # compute wavelet coefficients
     w = np.zeros(aw.wavelets.shape, dtype=np.complex128)
@@ -37,17 +35,8 @@ def main() -> None:
             ind = ssht.elm2ind(ell, m)
             w[:, ind] = wav_0 * earth_noised.multipole[ind]
 
-    # compute sigma for wavelets
-    sigma_j = np.apply_along_axis(
-        lambda p: sigma_noise * L * np.sqrt(signal_power(L, p)), 1, aw.wavelets[1:]
-    )
-
     # hard thresholding
-    for j in range(1, aw.wavelets.shape[0]):
-        f = ssht.inverse(w[j], L)
-        cond = np.abs(f) < N_SIGMA * sigma_j[j - 1]
-        f = np.where(cond, 0, f)
-        w[j] = ssht.forward(f, L)
+    w_denoised = hard_thresholding(L, w, sigma_j)
 
     # wavelet synthesis
     flm = np.zeros(L ** 2, dtype=np.complex128)
@@ -56,7 +45,7 @@ def main() -> None:
         wav_0 = np.sqrt((4 * np.pi) / (2 * ell + 1)) * aw.wavelets[:, ind_m0]
         for m in range(-ell, ell + 1):
             ind = ssht.elm2ind(ell, m)
-            flm[ind] = (w[:, ind] * wav_0).sum()
+            flm[ind] = (w_denoised[:, ind] * wav_0).sum()
 
     # compute SNR
     compute_snr(L, earth.multipole, flm - earth.multipole)
--- pys2sleplet/utils/noise.py
+++ pys2sleplet/utils/noise.py
@@ -5,7 +5,7 @@ from pys2sleplet.utils.logger import logger
 from pys2sleplet.utils.vars import RANDOM_SEED
 
 
-def signal_power(L: int, flm: np.ndarray) -> np.ndarray:
+def _signal_power(L: int, flm: np.ndarray) -> np.ndarray:
     """
     computes the power of the harmonic signal
     """
@@ -16,11 +16,18 @@ def compute_snr(L: int, signal: np.ndarray, noise: np.ndarray) -> None:
     """
     computes the signal to noise ratio
     """
-    snr = 10 * np.log10(signal_power(L, signal) / signal_power(L, noise))
+    snr = 10 * np.log10(_signal_power(L, signal) / _signal_power(L, noise))
     logger.info(f"Noise SNR: {snr:.2f}")
 
 
-def create_noise(L: int, signal: np.ndarray, snr_in: float = 10) -> np.ndarray:
+def _compute_sigma_noise(L: int, signal: np.ndarray, snr_in: float = 10) -> float:
+    """
+    compute the std dev of the noise
+    """
+    return np.sqrt(10 ** (-snr_in / 10) * _signal_power(L, signal))
+
+
+def create_noise(L: int, signal: np.ndarray) -> np.ndarray:
     """
     computes Gaussian white noise
     """
@@ -31,7 +38,7 @@ def create_noise(L: int, signal: np.ndarray, snr_in: float = 10) -> np.ndarray:
     nlm = np.zeros(L ** 2, dtype=np.complex128)
 
     # std dev of the noise
-    sigma_noise = np.sqrt(10 ** (-snr_in / 10) * signal_power(L, signal))
+    sigma_noise = _compute_sigma_noise(L, signal)
 
     # compute noise
     for ell in range(L):
@@ -48,3 +55,27 @@ def create_noise(L: int, signal: np.ndarray, snr_in: float = 10) -> np.ndarray:
     # compute SNR
     compute_snr(L, signal, nlm)
     return nlm
+
+
+def hard_thresholding(
+    L: int, wav_coeffs: np.ndarray, sigma_j: np.ndarray, n_sigma: int = 3
+) -> None:
+    """
+    set pixels in real space to zero if the magnitude is less than the threshold
+    """
+    for j in range(1, len(wav_coeffs)):
+        f = ssht.inverse(wav_coeffs[j], L)
+        cond = np.abs(f) < n_sigma * sigma_j[j - 1]
+        f = np.where(cond, 0, f)
+        wav_coeffs[j] = ssht.forward(f, L)
+    return wav_coeffs
+
+
+def compute_sigma_j(L: int, flm: np.ndarray, psi_j: np.ndarray) -> np.ndarray:
+    """
+    compute sigma_j for wavelets used in denoising the signal
+    """
+    sigma_noise = _compute_sigma_noise(L, flm)
+    return np.apply_along_axis(
+        lambda p: sigma_noise * L * np.sqrt(_signal_power(L, p)), 1, psi_j
+    )
