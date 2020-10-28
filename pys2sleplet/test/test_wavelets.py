ce5b440b8416beeaed3ac48717a56f69f314f047
paddyroddy <patrickjamesroddy@gmail.com>
2020-10-28 16:25:19 +0000

    Change test to Slepian space

--- pys2sleplet/test/test_wavelets.py
+++ pys2sleplet/test/test_wavelets.py
@@ -1,12 +1,10 @@
 import numpy as np
-import pyssht as ssht
 import pytest
 from numpy.testing import assert_allclose
 
 from pys2sleplet.functions.fp.slepian_wavelets import SlepianWavelets
 from pys2sleplet.test.constants import L_SMALL as L
-from pys2sleplet.utils.mask_methods import create_mask_region
-from pys2sleplet.utils.slepian_methods import slepian_forward, slepian_inverse
+from pys2sleplet.utils.slepian_methods import slepian_forward
 from pys2sleplet.utils.wavelet_methods import (
     slepian_wavelet_forward,
     slepian_wavelet_inverse,
@@ -22,10 +20,7 @@ def test_synthesis_polar(slepian_polar_cap, earth_polar_cap) -> None:
     coefficients = slepian_forward(L, earth_polar_cap.coefficients, slepian_polar_cap)
     wav_coeffs = slepian_wavelet_forward(coefficients, sw.wavelets, slepian_polar_cap.N)
     f_p = slepian_wavelet_inverse(wav_coeffs, sw.wavelets, slepian_polar_cap.N)
-    f_wavelets = slepian_inverse(L, f_p, slepian_polar_cap)
-    f_harmonic = ssht.inverse(earth_polar_cap.coefficients, L)
-    mask = create_mask_region(L, slepian_polar_cap.region)
-    assert_allclose(np.abs(f_wavelets - f_harmonic)[mask].mean(), 0, atol=271)
+    assert_allclose(np.abs(f_p - coefficients).mean(), 0, atol=1e-15)
 
 
 def test_synthesis_lim_lat_lon(slepian_lim_lat_lon, earth_lim_lat_lon) -> None:
@@ -40,7 +35,4 @@ def test_synthesis_lim_lat_lon(slepian_lim_lat_lon, earth_lim_lat_lon) -> None:
         coefficients, sw.wavelets, slepian_lim_lat_lon.N
     )
     f_p = slepian_wavelet_inverse(wav_coeffs, sw.wavelets, slepian_lim_lat_lon.N)
-    f_wavelets = slepian_inverse(L, f_p, slepian_lim_lat_lon)
-    f_harmonic = ssht.inverse(earth_lim_lat_lon.coefficients, L)
-    mask = create_mask_region(L, slepian_lim_lat_lon.region)
-    assert_allclose(np.abs(f_wavelets - f_harmonic)[mask].mean(), 0, atol=1e3)
+    assert_allclose(np.abs(f_p - coefficients).mean(), 0)
