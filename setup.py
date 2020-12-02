0af8bd42e4ae53307f89d1218cf6edb3b9130b8c
paddyroddy <patrickjamesroddy@gmail.com>
2020-12-02 11:17:45 +0000

    Bring in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from setuptools import find_namespace_packages, setup
+import os
 
-scripts = ["plotting=pys2sleplet.scripts.plotting:main"]
+from Cython.Build import cythonize
+from setuptools import Extension, find_namespace_packages, setup
+
+os.environ["CC"] = "gcc-10"
 
 setup(
     name="pys2sleplet",
@@ -9,5 +12,17 @@ setup(
     author_email="patrickjamesroddy@gmail.com",
     packages=find_namespace_packages(),
     include_package_data=True,
-    entry_points=dict(console_scripts=scripts),
+    entry_points=dict(console_scripts=["plotting=pys2sleplet.scripts.plotting:main"]),
+    ext_modules=cythonize(
+        Extension(
+            "slepian_computations",
+            ["pys2sleplet/cython/*.pyx"],
+            extra_compile_args=["-fopenmp"],
+            extra_link_args=["-fopenmp"],
+            include_dirs=["/usr/local/include"],
+        ),
+        annotate=True,
+        language_level=3,
+        compiler_directives=dict(boundscheck=False, embedsignature=True),
+    ),
 )
