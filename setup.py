03ccea10f7484916239f260019084ba228ac6697
paddyroddy <patrickjamesroddy@gmail.com>
2020-12-03 19:34:10 +0000

    Remove cython from build

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from Cython.Build import cythonize
-from setuptools import Extension, find_namespace_packages, setup
+from setuptools import find_namespace_packages, setup
 
 setup(
     name="pys2sleplet",
@@ -9,10 +8,4 @@ setup(
     packages=find_namespace_packages(),
     include_package_data=True,
     entry_points=dict(console_scripts=["plotting=pys2sleplet.scripts.plotting:main"]),
-    ext_modules=cythonize(
-        Extension("slepian_computations", ["pys2sleplet/cython/*.pyx"]),
-        annotate=True,
-        language_level=3,
-        compiler_directives=dict(boundscheck=False, embedsignature=True),
-    ),
 )
