16f0d90f7cd4c16ec4a5f01e5ded6c74616da55b
paddyroddy <patrickjamesroddy@gmail.com>
2020-12-17 19:18:03 +0000

    Add argparse for different sigma/snr values

--- pys2sleplet/plotting/arbitrary/denoising_slepian_south_america.py
+++ pys2sleplet/plotting/arbitrary/denoising_slepian_south_america.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+
 from pys2sleplet.plotting.create_plot import Plot
 from pys2sleplet.utils.denoising import denoising_slepian
 from pys2sleplet.utils.region import Region
@@ -9,18 +11,32 @@ N_SIGMA = 3
 SNR_IN = -10
 
 
-def main() -> None:
+def main(snr: int, sigma: float) -> None:
     """
     denoising demo using Slepian wavelets
     """
     fun = "south_america"
     region = Region(mask_name=fun)
     f, annotations = denoising_slepian(
-        f"slepian_{fun}", L, B, J_MIN, N_SIGMA, region, SNR_IN
+        f"slepian_{fun}", L, B, J_MIN, sigma, region, snr
     )
     name = f"{fun}_denoised_slepian_L{L}"
     Plot(f, L, name, annotations=annotations).execute()
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser(description="denoising")
+    parser.add_argument(
+        "--snr",
+        "-s",
+        type=int,
+        default=SNR_IN,
+    )
+    parser.add_argument(
+        "--sigma",
+        "-n",
+        type=float,
+        default=N_SIGMA,
+    )
+    args = parser.parse_args()
+    main(args.snr, args.sigma)
