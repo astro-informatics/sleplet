1b4a55934525161cc20bc9411993278cc7343905
paddyroddy <patrickjamesroddy@gmail.com>
2021-07-20 14:03:53 +0100

    Class to plot basis functions of mesh

---
+++ pys2sleplet/meshes/harmonic_coefficients/mesh_basis_functions.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass, field
+
+from pys2sleplet.meshes.mesh_harmonic_coefficients import MeshHarmonicCoefficients
+from pys2sleplet.utils.harmonic_methods import mesh_forward
+from pys2sleplet.utils.logger import logger
+
+
+@dataclass
+class MeshBasisFunctions(MeshHarmonicCoefficients):
+    rank: int
+    _rank: int = field(default=0, init=False, repr=False)
+
+    def __post_init__(self) -> None:
+        self._validate_rank()
+        super().__post_init__()
+
+    def _create_coefficients(self) -> None:
+        """
+        compute field on the vertices of the mesh
+        """
+        basis_function = self.mesh.basis_functions[self.rank]
+        self.coefficients = mesh_forward(self.mesh, basis_function)
+        logger.info(
+            f"Eigenvalue {self.rank}: {self.mesh.mesh_eigenvalues[self.rank]:e}"
+        )
+
+    def _create_name(self) -> str:
+        return (
+            (
+                f"{self.name}_rank{self.index}_"
+                f"lam{self.mesh.mesh_eigenvalues[self.index]:e}"
+            )
+            .replace(".", "-")
+            .replace("+", "")
+        )
+
+    def _setup_args(self) -> None:
+        if isinstance(self.extra_args, list):
+            num_args = 1
+            if len(self.extra_args) != num_args:
+                raise ValueError(
+                    f"The number of extra arguments should be 1 or {num_args}"
+                )
+            self.rank = self.extra_args[0]
+
+    def _validate_rank(self) -> None:
+        """
+        checks the requested rank is valid
+        """
+        if isinstance(self.extra_args, list):
+            limit = self.basis_functions.shape[0]
+            if self.extra_args[0] > limit:
+                raise ValueError(f"rank should be less than or equal to {limit}")
+
+    @property  # type:ignore
+    def rank(self) -> int:
+        return self._rank
+
+    @rank.setter
+    def rank(self, rank: int) -> None:
+        if isinstance(rank, property):
+            # initial value not specified, use default
+            # https://stackoverflow.com/a/61480946/7359333
+            rank = MeshBasisFunctions._rank
+        if not isinstance(rank, int):
+            raise TypeError("rank should be an integer")
+        if rank < 0:
+            raise ValueError("rank cannot be negative")
+        self._rank = rank
