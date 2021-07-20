aaf3afe7ee71428386297b8b3177356a03027878
paddyroddy <patrickjamesroddy@gmail.com>
2021-07-20 15:18:36 +0100

    Add Slepian function plotting fun

---
+++ pys2sleplet/meshes/slepian_coefficients/slepian_mesh_functions.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass, field
+
+from pys2sleplet.meshes.mesh_slepian_coefficients import MeshSlepianCoefficients
+from pys2sleplet.utils.logger import logger
+from pys2sleplet.utils.slepian_methods import slepian_mesh_forward
+
+
+@dataclass
+class SlepianMeshFunctions(MeshSlepianCoefficients):
+    rank: int
+    _rank: int = field(default=0, init=False, repr=False)
+
+    def __post_init__(self) -> None:
+        super().__post_init__()
+
+    def _create_coefficients(self) -> None:
+        """
+        compute field on the vertices of the mesh
+        """
+        s_p_i = self.slepian_mesh.slepian_functions[self.rank]
+        self.coefficients = slepian_mesh_forward(
+            self.slepian_mesh,
+            u_i=s_p_i,
+        )
+        logger.info(
+            f"Slepian eigenvalue {self.rank}: "
+            f"{self.slepian_mesh.slepian_eigenvalues[self.rank]:e}"
+        )
+
+    def _create_name(self) -> str:
+        return (
+            (
+                f"slepian_{self.name}_rank{self.index}_"
+                f"lam{self.slepian_mesh.slepian_eigenvalues[self.index]:e}"
+            )
+            .replace(".", "-")
+            .replace("+", "")
+        )
+
+    def _setup_args(self) -> None:
+        if isinstance(self.extra_args, list):
+            num_args = 1
+            if len(self.extra_args) != num_args:
+                raise ValueError(
+                    f"The number of extra arguments should be 1 or {num_args}"
+                )
+            self.rank = self.extra_args[0]
+
+    def _validate_rank(self) -> None:
+        """
+        checks the requested rank is valid
+        """
+        if isinstance(self.extra_args, list):
+            limit = self.basis_functions.shape[0]
+            if self.extra_args[0] > limit:
+                raise ValueError(f"rank should be less than or equal to {limit}")
+
+    @property  # type:ignore
+    def rank(self) -> int:
+        return self._rank
+
+    @rank.setter
+    def rank(self, rank: int) -> None:
+        if isinstance(rank, property):
+            # initial value not specified, use default
+            # https://stackoverflow.com/a/61480946/7359333
+            rank = SlepianMeshFunctions._rank
+        if not isinstance(rank, int):
+            raise TypeError("rank should be an integer")
+        if rank < 0:
+            raise ValueError("rank cannot be negative")
+        self._rank = rank
