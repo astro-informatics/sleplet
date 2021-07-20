1b2ee5bc98f509dde3048b4cb43507adf13eb058
paddyroddy <patrickjamesroddy@gmail.com>
2021-07-20 14:27:57 +0100

    Create Slepian mesh wavelets class

--- pys2sleplet/meshes/slepian_coefficients/slepian_mesh_wavelets.py
+++ pys2sleplet/meshes/slepian_coefficients/slepian_mesh_wavelets.py
@@ -1,23 +1,50 @@
 from dataclasses import dataclass, field
+from typing import Optional
 
 import numpy as np
+from pys2let import pys2let_j_max
 
-from pys2sleplet.meshes.classes.slepian_mesh import SlepianMesh
+from pys2sleplet.meshes.mesh_slepian_coefficients import MeshSlepianCoefficients
+from pys2sleplet.utils.logger import logger
+from pys2sleplet.utils.string_methods import filename_args, wavelet_ending
 from pys2sleplet.utils.wavelet_methods import create_kappas
 
 
 @dataclass
-class SlepianMeshWavelets:
-    slepian_mesh: SlepianMesh
+class SlepianMeshWavelets(MeshSlepianCoefficients):
     B: int
     j_min: int
+    j: Optional[int]
     _B: int = field(default=3, init=False, repr=False)
+    _j: Optional[int] = field(default=None, init=False, repr=False)
+    _j_max: int = field(init=False, repr=False)
     _j_min: int = field(default=2, init=False, repr=False)
-    _slepian_mesh: SlepianMesh = field(init=False, repr=False)
     _wavelets: np.ndarray = field(init=False, repr=False)
 
     def __post_init__(self) -> None:
+        super().__post_init__()
+
+    def _create_coefficients(self) -> None:
+        logger.info("start computing wavelets")
         self._create_wavelets()
+        logger.info("finish computing wavelets")
+        jth = 0 if self.j is None else self.j + 1
+        self.coefficients = self.wavelets[jth]
+
+    def _create_name(self) -> str:
+        return (
+            f"slepian_wavelets_{self.name}"
+            f"{filename_args(self.B, 'B')}"
+            f"{filename_args(self.j_min, 'jmin')}"
+            f"{wavelet_ending(self.j_min, self.j)}"
+        )
+
+    def _setup_args(self) -> None:
+        if isinstance(self.extra_args, list):
+            num_args = 3
+            if len(self.extra_args) != num_args:
+                raise ValueError(f"The number of extra arguments should be {num_args}")
+            self.B, self.j_min, self.j = self.extra_args[:num_args]
 
     def _create_wavelets(self) -> None:
         """
@@ -40,6 +67,33 @@ class SlepianMeshWavelets:
         self._B = B
 
     @property  # type:ignore
+    def j(self) -> Optional[int]:
+        return self._j
+
+    @j.setter
+    def j(self, j: Optional[int]) -> None:
+        if isinstance(j, property):
+            # initial value not specified, use default
+            # https://stackoverflow.com/a/61480946/7359333
+            j = SlepianMeshWavelets._j
+        self.j_max = pys2let_j_max(self.B, self.L ** 2, self.j_min)
+        if j is not None and j < 0:
+            raise ValueError("j should be positive")
+        if j is not None and j > self.j_max - self.j_min:
+            raise ValueError(
+                f"j should be less than j_max - j_min: {self.j_max - self.j_min + 1}"
+            )
+        self._j = j
+
+    @property
+    def j_max(self) -> int:
+        return self._j_max
+
+    @j_max.setter
+    def j_max(self, j_max: int) -> None:
+        self._j_max = j_max
+
+    @property  # type:ignore
     def j_min(self) -> int:
         return self._j_min
 
@@ -50,14 +104,6 @@ class SlepianMeshWavelets:
             # https://stackoverflow.com/a/61480946/7359333
             j_min = SlepianMeshWavelets._j_min
         self._j_min = j_min
-
-    @property  # type:ignore
-    def slepian_mesh(self) -> SlepianMesh:
-        return self._slepian_mesh
-
-    @slepian_mesh.setter
-    def slepian_mesh(self, slepian_mesh: SlepianMesh) -> None:
-        self._slepian_mesh = slepian_mesh
 
     @property
     def wavelets(self) -> np.ndarray:
