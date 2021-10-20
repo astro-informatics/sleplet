1ae109c9ebda462fc242d55b40fdb1d923dadcd9
paddyroddy <patrickjamesroddy@gmail.com>
2021-10-20 22:04:36 +0100

    Axisymmetric tiling

---
+++ pys2sleplet/plotting/wavelets/axisymmetric_tiling.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+from scipy.interpolate import pchip
+
+from pys2sleplet.utils.plot_methods import save_plot
+from pys2sleplet.utils.wavelet_methods import create_kappas
+
+file_location = Path(__file__).resolve()
+fig_path = file_location.parents[2] / "figures"
+sns.set(context="paper")
+
+B = 3
+J_MIN = 2
+L = 128
+STEP = 0.01
+
+
+def main() -> None:
+    """
+    plots the tiling of the harmonic line
+    """
+    xlim = L
+    x = np.arange(xlim)
+    xi = np.arange(0, xlim - 1 + STEP, STEP)
+    kappas = create_kappas(xlim, B, J_MIN)
+    yi = pchip(x, kappas[0])
+    plt.semilogx(xi, yi(xi), label=r"$\Phi_p$")
+    for j, k in enumerate(kappas[1:]):
+        yi = pchip(x, k)
+        plt.semilogx(xi, yi(xi), label=rf"$\Psi^{{{j+J_MIN}}}_p$")
+    plt.xlim(1, xlim)
+    ticks = 2 ** np.arange(np.log2(xlim) + 1, dtype=int)
+    plt.xticks(ticks, ticks)
+    plt.xlabel(r"$\ell$")
+    plt.legend()
+    save_plot(fig_path, f"polar_cap_tiling_L{L}")
+
+
+if __name__ == "__main__":
+    main()
