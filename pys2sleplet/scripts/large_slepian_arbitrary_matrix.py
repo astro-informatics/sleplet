9a2dfd0be8389336e5856506fb21799706c70b03
paddyroddy <patrickjamesroddy@gmail.com>
2022-07-29 21:28:52 +0100

    Revert "Make script work for full spectrum too"
    This reverts commit fec97cf9ea2b7b8da679b6b4974058f1596a29b9.

--- pys2sleplet/scripts/large_slepian_arbitrary_matrix.py
+++ pys2sleplet/scripts/large_slepian_arbitrary_matrix.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import numpy as np
 from numpy import linalg as LA
 from numpy.testing import assert_equal
-from typing import Optional
 
 from pys2sleplet.utils.slepian_arbitrary_methods import (
     calculate_high_L_matrix,
@@ -15,18 +14,13 @@ _eigen_path = _file_location.parents[1] / "data" / "slepian" / "eigensolutions"
 
 
 def compute_large_D_matrix(
-    mask_name: str, *, L: int, L_ranges: list[int], shannon: Optional[int] = None
+    mask_name: str, L: int, L_ranges: list[int], shannon: int
 ) -> None:
     """
     checks that the split up D matrix has the same eigenvalues
     & eigenvectors as the computation of the whole D matrix in one step
     """
-    # create filename
-    filename = f"D_{mask_name}_L{L}"
-    if shannon is not None:
-        filename += f"_N{shannon}"
-    slepian_loc = _eigen_path / filename
-
+    slepian_loc = _eigen_path / f"D_{mask_name}_L{L}_N{shannon}"
     D = calculate_high_L_matrix(slepian_loc, L, L_ranges)
     eigenvalues_split, eigenvectors_split = clean_evals_and_evecs(LA.eigh(D))
 
@@ -38,12 +32,11 @@ def compute_large_D_matrix(
         assert_equal(eigenvalues_split, eigenvalues)
         assert_equal(eigenvectors_split, eigenvectors)
     else:
-        n_save = len(D) if shannon is None else shannon
-        np.save(eval_loc, eigenvalues_split[:n_save])
-        np.save(evec_loc, eigenvectors_split[:n_save])
+        np.save(eval_loc, eigenvalues_split[:shannon])
+        np.save(evec_loc, eigenvectors_split[:shannon])
 
 
 if __name__ == "__main__":
     compute_large_D_matrix(
-        "south_america", L=128, L_ranges=[0, 128]
+        mask_name="south_america", L=128, L_ranges=[0, 64, 128], shannon=678
     )
