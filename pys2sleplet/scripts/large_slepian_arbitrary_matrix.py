fec97cf9ea2b7b8da679b6b4974058f1596a29b9
paddyroddy <patrickjamesroddy@gmail.com>
2022-07-29 20:54:21 +0100

    Make script work for full spectrum too

--- pys2sleplet/scripts/large_slepian_arbitrary_matrix.py
+++ pys2sleplet/scripts/large_slepian_arbitrary_matrix.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 from numpy import linalg as LA
 from numpy.testing import assert_equal
+from typing import Optional
 
 from pys2sleplet.utils.slepian_arbitrary_methods import (
     calculate_high_L_matrix,
@@ -14,13 +15,18 @@ _eigen_path = _file_location.parents[1] / "data" / "slepian" / "eigensolutions"
 
 
 def compute_large_D_matrix(
-    mask_name: str, L: int, L_ranges: list[int], shannon: int
+    mask_name: str, *, L: int, L_ranges: list[int], shannon: Optional[int] = None
 ) -> None:
     """
     checks that the split up D matrix has the same eigenvalues
     & eigenvectors as the computation of the whole D matrix in one step
     """
-    slepian_loc = _eigen_path / f"D_{mask_name}_L{L}_N{shannon}"
+    # create filename
+    filename = f"D_{mask_name}_L{L}"
+    if shannon is not None:
+        filename += f"_N{shannon}"
+    slepian_loc = _eigen_path / filename
+
     D = calculate_high_L_matrix(slepian_loc, L, L_ranges)
     eigenvalues_split, eigenvectors_split = clean_evals_and_evecs(LA.eigh(D))
 
@@ -32,11 +38,12 @@ def compute_large_D_matrix(
         assert_equal(eigenvalues_split, eigenvalues)
         assert_equal(eigenvectors_split, eigenvectors)
     else:
-        np.save(eval_loc, eigenvalues_split[:shannon])
-        np.save(evec_loc, eigenvectors_split[:shannon])
+        n_save = len(D) if shannon is None else shannon
+        np.save(eval_loc, eigenvalues_split[:n_save])
+        np.save(evec_loc, eigenvectors_split[:n_save])
 
 
 if __name__ == "__main__":
     compute_large_D_matrix(
-        mask_name="south_america", L=128, L_ranges=[0, 64, 128], shannon=678
+        "south_america", L=128, L_ranges=[0, 128]
     )
