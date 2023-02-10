4ea681eca8aa2451d36081c4f839ccde9729efb1
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-02-10 11:18:11 +0000

    Fix type

--- sleplet/data/other/wmap/create_wmap_flm.py
+++ sleplet/data/other/wmap/create_wmap_flm.py
@@ -41,7 +41,7 @@ def create_flm(L: int) -> npt.NDArray[np.complex_]:
 
 def _load_cl(
     *, file_ending: str = "_lcdm_pl_model_wmap7baoh0"
-) -> npt.NDArray[np.complex_]:
+) -> npt.NDArray[np.float_]:
     """
     pick coefficients from file options are:
     * _lcdm_pl_model_yr1_v1.mat
