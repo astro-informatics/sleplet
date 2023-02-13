bce6afe66b6ebe2515f4d7846009d4f70ce1283a
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-02-13 17:52:09 +0000

    Move `conftest` to test directory

--- sleplet/test/conftest.py
+++ sleplet/test/conftest.py
@@ -30,26 +30,6 @@ from sleplet.utils.harmonic_methods import compute_random_signal
 from sleplet.utils.vars import RANDOM_SEED
 
 
-def pytest_addoption(parser) -> None:
-    parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-
-
-def pytest_configure(config) -> None:
-    config.addinivalue_line("markers", "slow: mark test as slow to run")
-
-
-def pytest_collection_modifyitems(config, items) -> None:
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
-
-
 @pytest.fixture(scope="session")
 def slepian_polar_cap() -> SlepianPolarCap:
     """
