904817dbeb8c78d38c9ebcb6747c0b1203be6d2e
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-03-30 11:13:59 +0100

    Keep init files

--- src/sleplet/meshes/__init__.py
+++ src/sleplet/meshes/__init__.py
@@ -1,3 +1,11 @@
 """
 classes to create and handle mesh (manifold) data
 """
+
+from ._mesh import Mesh
+from ._mesh_slepian import MeshSlepian
+
+__all__ = [
+    "Mesh",
+    "MeshSlepian",
+]
--- src/sleplet/meshes/slepian_coefficients/__init__.py
+++ src/sleplet/meshes/slepian_coefficients/__init__.py
@@ -1,3 +1,17 @@
 """
 classes of functions on the sphere created in Fourier space
 """
+
+from ._mesh_slepian_field import MeshSlepianField
+from ._mesh_slepian_functions import MeshSlepianFunctions
+from ._mesh_slepian_noise_field import MeshSlepianNoiseField
+from ._mesh_slepian_wavelet_coefficients import MeshSlepianWaveletCoefficients
+from ._mesh_slepian_wavelets import MeshSlepianWavelets
+
+__all__ = [
+    "MeshSlepianField",
+    "MeshSlepianFunctions",
+    "MeshSlepianNoiseField",
+    "MeshSlepianWaveletCoefficients",
+    "MeshSlepianWavelets",
+]
