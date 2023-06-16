204ea06edb3d0b0e6e4fbdac0a3c3ed08911b06f
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-06-16 12:15:52 +0100

    Restore one arb test

--- tests/test_arbitrary_region.py
+++ tests/test_arbitrary_region.py
@@ -1,35 +1,35 @@
-# import numpy as np
-# import pytest
-# from numpy.testing import assert_allclose
+import numpy as np
+import pytest
+from numpy.testing import assert_allclose
 
-# import sleplet
+import sleplet
 
 
-# @pytest.mark.slow()
-# def test_equality_to_polar_cap_method(slepian_polar_cap) -> None:
-#     """
-#     Tests that the eigenvectors and eigenvalues are close
-#     in comparison to the smarter Slepian polar cap method.
-#     """
-#     mask_name = slepian_polar_cap.region.name_ending
-#     slepian = sleplet.slepian.SlepianArbitrary(slepian_polar_cap.L, mask_name)
-#     assert_allclose(np.abs(slepian_polar_cap.N - slepian.N), 0, atol=3)
-#     assert_allclose(
-#         np.abs(
-#             slepian.eigenvalues[: slepian_polar_cap.N]
-#             - slepian_polar_cap.eigenvalues[: slepian_polar_cap.N],
-#         ).mean(),
-#         0,
-#         atol=0.88,
-#     )
-#     assert_allclose(
-#         np.abs(
-#             slepian.eigenvectors[: slepian_polar_cap.N]
-#             - slepian_polar_cap.eigenvectors[: slepian_polar_cap.N],
-#         ).mean(),
-#         0,
-#         atol=0.03,
-#     )
+@pytest.mark.slow()
+def test_equality_to_polar_cap_method(slepian_polar_cap) -> None:
+    """
+    Tests that the eigenvectors and eigenvalues are close
+    in comparison to the smarter Slepian polar cap method.
+    """
+    mask_name = slepian_polar_cap.region.name_ending
+    slepian = sleplet.slepian.SlepianArbitrary(slepian_polar_cap.L, mask_name)
+    assert_allclose(np.abs(slepian_polar_cap.N - slepian.N), 0, atol=3)
+    assert_allclose(
+        np.abs(
+            slepian.eigenvalues[: slepian_polar_cap.N]
+            - slepian_polar_cap.eigenvalues[: slepian_polar_cap.N],
+        ).mean(),
+        0,
+        atol=0.88,
+    )
+    assert_allclose(
+        np.abs(
+            slepian.eigenvectors[: slepian_polar_cap.N]
+            - slepian_polar_cap.eigenvectors[: slepian_polar_cap.N],
+        ).mean(),
+        0,
+        atol=0.03,
+    )
 
 
 # @pytest.mark.slow()
