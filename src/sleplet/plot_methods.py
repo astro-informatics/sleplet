2ac486b71b2522cbe51bb1f3116ad9f92d068e15
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-09-19 14:42:38 +0100

    Fix grid point test

--- src/sleplet/plot_methods.py
+++ src/sleplet/plot_methods.py
@@ -66,14 +66,8 @@ def _calc_nearest_grid_point(
     values - the translation needs to be at the same position
     as the rotation such that the difference error is small.
     """
-    thetas = np.tile(
-        s2fft.samples.thetas(L, sampling=sleplet._vars.SAMPLING_SCHEME),
-        (s2fft.samples.nphi_equiang(L, sampling=sleplet._vars.SAMPLING_SCHEME), 1),
-    ).T
-    phis = np.tile(
-        s2fft.samples.phis_equiang(L, sampling=sleplet._vars.SAMPLING_SCHEME),
-        (s2fft.samples.ntheta(L, sampling=sleplet._vars.SAMPLING_SCHEME), 1),
-    )
+    thetas = s2fft.samples.thetas(L, sampling=sleplet._vars.SAMPLING_SCHEME)
+    phis = s2fft.samples.phis_equiang(L, sampling=sleplet._vars.SAMPLING_SCHEME)
     pix_j = np.abs(phis - alpha_pi_fraction * np.pi).argmin()
     pix_i = np.abs(thetas - beta_pi_fraction * np.pi).argmin()
     alpha, beta = phis[pix_j], thetas[pix_i]
