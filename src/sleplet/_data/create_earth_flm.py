0ac465a2343871c62b1b25a783b2c8d9110f0511
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-10-04 21:47:21 +0100

    Invert flm manually

--- src/sleplet/_data/create_earth_flm.py
+++ src/sleplet/_data/create_earth_flm.py
@@ -12,21 +12,18 @@ import sleplet._smoothing
 def create_flm(L: int, *, smoothing: int | None = None) -> npt.NDArray[np.complex_]:
     """Creates the flm for the whole Earth."""
     # load in data
-    flm_full = _load_flm()
+    flm = _load_flm()
 
     # don't take the full L, and convert to 2D
-    flm_2d = s2fft.samples.flm_1d_to_2d(flm_full[: L**2], L)
+    flm = s2fft.samples.flm_1d_to_2d(flm[: L**2], L)
 
     # fill in negative m components so as to avoid confusion with zero values
     for ell in range(1, L):
         for m in range(1, ell + 1):
-            flm_2d[ell, L - 1 - m] = (-1) ** m * flm_2d[
-                ell,
-                L - 1 + m,
-            ].conj()
+            # invert dataset as Earth backwards
+            flm[ell, L - 1 + m] = (-1) ** m * flm[ell, L - 1 - m].conj()
 
-    # invert dataset as Earth backwards
-    flm = s2fft.samples.flm_2d_to_1d(flm_2d.conj(), L)
+    flm = s2fft.samples.flm_2d_to_1d(flm, L)
 
     if isinstance(smoothing, int):
         flm = sleplet._smoothing.apply_gaussian_smoothing(flm, L, smoothing)
