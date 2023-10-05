b1bbca50948305d21abaa11ea10d612c6f5029cd
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-10-05 12:59:06 +0100

    Remove s2wav

--- src/sleplet/functions/axisymmetric_wavelet_coefficients_south_america.py
+++ src/sleplet/functions/axisymmetric_wavelet_coefficients_south_america.py
@@ -4,7 +4,6 @@ import logging
 import numpy as np
 import numpy.typing as npt
 import pydantic.v1 as pydantic
-import s2wav
 
 import sleplet._string_methods
 import sleplet._validation
--- src/sleplet/functions/ridgelets.py
+++ src/sleplet/functions/ridgelets.py
@@ -4,7 +4,6 @@ import logging
 import numpy as np
 import numpy.typing as npt
 import pydantic.v1 as pydantic
-import s2wav
 import scipy.special
 
 import sleplet._string_methods
