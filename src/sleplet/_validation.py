1069ba1acf215c053419a23a04e631bdd442a758
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-10-07 16:57:04 +0100

    Fix spherical plots

--- src/sleplet/_validation.py
+++ src/sleplet/_validation.py
@@ -1,5 +1,15 @@
+import pydantic
+
+
 class Validation:
     arbitrary_types_allowed = True
     smart_union = True
     validate_all = True
     validate_assignment = True
+
+
+validation = pydantic.ConfigDict(
+    arbitrary_types_allowed=True,
+    validate_assignment=True,
+    validate_default=True,
+)
