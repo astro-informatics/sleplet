59d91bc30faed54250e1356f324920ec02fef188
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-08-08 18:37:13 +0100

    Fix linting

--- src/sleplet/meshes/mesh_slepian_wavelets.py
+++ src/sleplet/meshes/mesh_slepian_wavelets.py
@@ -67,7 +67,7 @@ class MeshSlepianWavelets(MeshSlepianCoefficients):
 
     @pydantic.field_validator("j")
     def _check_j(
-        cls,  # noqa: ANN101
+        cls,
         v: int | None,
         info: pydantic.ValidationInfo,
     ) -> int | None:
