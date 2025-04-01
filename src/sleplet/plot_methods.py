38ef95e759de319ccb4417d7d44b858c4644629e
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-04-01 17:32:12 +0100

    Revert "Attempt to fix colourscale"
    This reverts commit dd459c47092a6278454b0e202b830a3867b44d7c.

--- src/sleplet/plot_methods.py
+++ src/sleplet/plot_methods.py
@@ -45,14 +45,14 @@ def _convert_colourscale(
     cmap: mpl.colors.LinearSegmentedColormap,
     *,
     pl_entries: int = 255,
-) -> list[list[float | str]]:
+) -> list[tuple[float, str]]:
     """Convert cmocean colourscale to a plotly colourscale."""
     h = 1 / (pl_entries - 1)
-    pl_colorscale: list[list[float | str]] = []
+    pl_colorscale = []
 
     for k in range(pl_entries):
         C = list(map(np.uint8, np.array(cmap(k * h)[:3]) * 255))
-        pl_colorscale.append([k * h, f"rgb{(C[0], C[1], C[2])}"])
+        pl_colorscale.append((k * h, f"rgb{(C[0], C[1], C[2])}"))
 
     return pl_colorscale
 
